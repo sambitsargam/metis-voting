1ca3d86e9ca733885ac8179f75a9621154d88190
SAMBIT SARGAM EKALABAYA <sambitsargam2003@gmail.com>
2022-04-10 00:45:20 +0530

    upload files

---
+++ components/Icons.js
@@ -0,0 +1,35 @@
+import LightningIcon from "../public/svg/lightning.svg"
+import FillPollIcon from "../public/svg/Fill Poll Icon.svg"
+import votingdappHorizontalIcon from "../public/svg/votechain Logo.png"
+import LoginIcon from "../public/svg/Login.png"
+import HistoryIcon from "../public/svg/time.svg"
+import DashboardIcon from "../public/svg/Dashboard Icon.svg"
+import ToggleIcon from "../public/svg/Toggle Button.svg"
+import CreatePollIcon from "../public/svg/Create Poll Icon.svg"
+import LogoIcon from "../public/svg/logo.png"
+import BarChartCircleIcon from "../public/svg/barchart.svg"
+import RadialSmallIcon from "../public/svg/radial-small.svg"
+import RadialIcon from "../public/svg/radial.svg"
+import ZapIcon from "../public/svg/Zap.svg"
+import RateIcon from "../public/svg/rate.svg"
+import RadioCircleIcon from "../public/svg/radio-circle.svg"
+import RadioSelectedIcon from "../public/svg/radio-selected.svg"
+import Image from "next/image"
+
+export const Lightning = <Image src={LightningIcon} alt=""/>
+export const votingdappHorizontal = <Image src={votingdappHorizontalIcon} width={150} height={80} alt=""/>
+export const Login = <Image src={LoginIcon} alt=""/>
+export const Logo = <Image src={LogoIcon} alt=""/>
+export const BarChartCircle = <Image src={BarChartCircleIcon} alt=""/>
+export const RadialSmall = <Image src={RadialSmallIcon} alt=""/>
+export const Radial = <Image src={RadialIcon} alt=""/>
+export const Zap = <Image src={ZapIcon} alt=""/>
+export const Rate = <Image src={RateIcon} alt=""/>
+export const RadioCircle = <Image src={RadioCircleIcon} alt=""/>
+export const RadioSelected = <Image src={RadioSelectedIcon} alt=""/>
+
+export const Dashboard = <Image src={DashboardIcon} alt=""/>
+export const Toggle = <Image src={ToggleIcon} alt=""/>
+export const CreatePoll = <Image src={CreatePollIcon} alt=""/>
+export const FillPoll = <Image src={FillPollIcon} alt=""/>
+export const History = <Image src={HistoryIcon} alt=""/>
